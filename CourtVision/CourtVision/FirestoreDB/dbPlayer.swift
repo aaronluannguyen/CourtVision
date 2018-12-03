87dd7ee7d259f2caa317150189534bb5be435169
aaronluannguyen <aaronluannguyen@gmail.com>
2018-12-03 01:10:22 -0800

    Added code for querying player data from Firestore. It's commented out in player class

--- CourtVision/CourtVision/FirestoreDB/dbPlayer.swift
+++ CourtVision/CourtVision/FirestoreDB/dbPlayer.swift
@@ -13,7 +13,9 @@ import Firebase
 public class PlayerDM {
   var playerObj: [String : Any]
   var userID: String
+
   
+  //Constructor for new player creation
   init(_ userID: String, _ email: String, _ position: String, _ addCode: String) {
     self.playerObj = [
       "profile": [
@@ -31,6 +33,13 @@ public class PlayerDM {
     self.userID = userID
   }
   
+  //Constructor for reading in player from Firestore
+  init(_ data: [String : Any], _ userID: String) {
+    self.playerObj = data
+    
+    self.userID = userID
+  }
+  
   //Initializes new player into Firestore db
   func newPlayer() {
     var db: Firestore!
@@ -40,8 +49,30 @@ public class PlayerDM {
     db.collection(FirebaseConstants.shared.playersCollection).document(self.userID).setData(self.playerObj) {err in
       if let err = err {
         print(err.localizedDescription)
-        //      self.signupErrorAlert("Firebase Error", "Player insertion into database error. " + err.localizedDescription)
+        //self.signupErrorAlert("Firebase Error", "Player insertion into database error. " + err.localizedDescription)
       }
     }
   }
 }
+
+//example of querying for player and then accessing nested dictionaries.
+//
+//var db: Firestore!
+//Firestore.firestore().settings = FirestoreSettings()
+//db = Firestore.firestore()
+//
+//let docRef = db.collection("players").document("pRwp8j8TpofbTCfZqywEfwHPiuD3")
+//
+//docRef.getDocument { (document, error) in
+//  if let city = document.flatMap({
+//    $0.data().flatMap({ (data) in
+//      return PlayerDM(data, "pRwp8j8TpofbTCfZqywEfwHPiuD3")
+//    })
+//  }) {
+//    print("City: \(city)")
+//    let profile = city.playerObj["profile"] as! [String:Any]
+//    print(profile["email"])
+//  } else {
+//    print("Document does not exist")
+//  }
+//}
