dfd922c5d280e404d19eb02e97a1ff870b5b1395
Wynston <wynhsu@uw.edu>
2018-12-02 20:55:48 -0800

    tab view setup; working on profile

---
+++ CourtVision/CourtVision/TabBarViews/ProfileViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ProfileViewController.swift
+//  CourtVision
+//
+//  Created by Wynston Hsu on 12/2/18.
+//  Copyright © 2018 Aaron Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- CourtVision/CourtVision/ViewControllers/BrowseTabBarController.swift
+++ CourtVision/CourtVision/ViewControllers/BrowseTabBarController.swift
@@ -1,8 +1,9 @@
 //
 //  BrowseTabBarController.swift
-//  
+//  CourtVision
 //
 //  Created by Wynston Hsu on 12/2/18.
+//  Copyright © 2018 Aaron Nguyen. All rights reserved.
 //
 
 import UIKit
